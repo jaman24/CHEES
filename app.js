37b5ea9a7345f00ae90e118f43a3a4d039a47eb0
JAMAN KHAN <jamankhanjk5@gmail.com>
2024-05-18 21:22:32 +0600

    DragDrop funtionality

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const gameBoard = document.getElementById("gameboard");
-const playerDisplay = document.querySelector("player");
-const infoDisplay = document.querySelector("infodisplay");
+const playerDisplay = document.getElementById("player");
+const infoDisplay = document.getElementById("infodisplay");
 
 const width = 8;
+let playerGo = "black";
+playerDisplay.textContent = "black";
 
 const startPieces = [
   rook,
@@ -75,7 +77,7 @@ function createBoard() {
     const square = document.createElement("div");
     square.classList.add("square");
     square.innerHTML = startPiece;
-    square.firstChild?.setAttribute("dragable", true);
+    square.firstChild?.setAttribute("draggable", true);
     square.setAttribute("square-id", i);
     // square.classList.add("beige");
     const row = Math.floor((63 - i) / 8) + 1;
@@ -95,3 +97,59 @@ function createBoard() {
 }
 
 createBoard();
+
+const allSquares = document.querySelectorAll("#gameboard .square");
+
+allSquares.forEach((square) => {
+  square.addEventListener("dragstart", dragStart);
+  square.addEventListener("dragover", dragOver);
+  square.addEventListener("drop", dragDrop);
+});
+
+let startPositionId;
+let draggedElement;
+
+function dragStart(e) {
+  startPositionId = e.target.parentNode.getAttribute("square-id");
+  draggedElement = e.target;
+}
+
+function dragOver(e) {
+  e.preventDefault();
+  // console.log(e.target);
+}
+
+function dragDrop(e) {
+  e.stopPropagation();
+  // console.log(e.target);
+  const taken = e.target.classList.contains("piece");
+
+  // e.target.parentNode.append(draggedElement);
+  // console.log(e.target);
+  // e.target.append(draggedElement);
+  // e.target.remove();
+  changePlayer();
+}
+
+function changePlayer() {
+  if (playerGo === "black") {
+    reverseIds();
+    playerGo = "white";
+    playerDisplay.textContent = "white";
+  } else {
+    revertIds();
+    playerGo = "black";
+    playerDisplay.textContent = "black";
+  }
+}
+
+function reverseIds() {
+  const allSquares = document.querySelectorAll(".square");
+  allSquares.forEach((square, i) => {
+    square.setAttribute("square-id", width * width - 1 - i);
+  });
+}
+function revertIds() {
+  const allSquares = document.querySelectorAll(".square");
+  allSquares.forEach((square, i) => square.setAttribute("square-id", i));
+}
